5b1c63f056048df49ef1eeb6d005ea15982490bf
Sliweczkowa <sliweczkowasliweczka@outlook.com>
2023-12-30 01:35:40 +0100

    fix: Place class Room annotations

--- app/src/main/java/com/example/helloworld/Place.kt
+++ app/src/main/java/com/example/helloworld/Place.kt
@@ -1,14 +1,28 @@
 package com.example.helloworld
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 
-@Entity
+@Entity(tableName = "place")
 data class Place (
+
+    @PrimaryKey(autoGenerate = true)
     var placeId: Int,
+
+    @ColumnInfo(name = "name")
     var placeName: String,
+
+    @ColumnInfo(name = "date")
     var date: String,
+
+    @ColumnInfo(name = "rating")
     var rating: Int,
+
+    @ColumnInfo(name = "description")
     var description: String,
+
+    @ColumnInfo(name = "photo")
     var photoPath: String = ""
 )
