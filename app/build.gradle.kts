c049e4f8e992abd402dc5d4456b4a93ca585f5ff
Sliweczkowa <sliweczkowasliweczka@outlook.com>
2024-01-07 14:22:40 +0100

    feat: dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,30 +1,27 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
+    id("com.squareup.sort-dependencies")
+    id("kotlin-android")
+    id("org.jmailen.kotlinter")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.helloworld"
-    compileSdk = 33
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.example.helloworld"
-        minSdk = 21
-        targetSdk = 33
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.compileSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "util.CustomTestRunner"
         vectorDrawables {
             useSupportLibrary = true
-        }
-
-        javaCompileOptions {
-            annotationProcessorOptions {
-                arguments["room.schemaLocation"] = "$projectDir/schemas"
-                arguments["room.incremental"] = true.toString()
-            }
         }
     }
 
@@ -34,55 +31,88 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    namespace = "com.example.helloworld"
 }
 
 dependencies {
+    ksp(libs.androidx.room.compiler)
+    ksp(libs.hilt.compiler)
+    ksp(libs.square.moshi.kotlin.codegen)
 
-    // Room
-    implementation("androidx.room:room-runtime:2.3.0")
-    kapt("androidx.room:room-compiler:2.3.0")
+    kspAndroidTest(libs.hilt.android.compiler)
 
-    // Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:2.3.0")
+    implementation(platform(libs.compose.bom))
+    implementation(libs.accompanist.systemuicontroller)
+    implementation(libs.android.material)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.compose.material)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling)
+    implementation(libs.hilt.android)
+    implementation(libs.square.moshi.kotlin)
+    implementation(libs.square.retrofit)
+    implementation(libs.square.retrofit.converter.moshi)
+    // google maps
+    implementation(libs.google.maps)
+    implementation(libs.google.maps.utils)
+    implementation(libs.google.maps.widgets)
 
-    //Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.38.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.37")
-    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha04")
+
+
+    implementation(libs.coil.ktx)
+
+    // navigation
+    implementation(libs.androidx.core.navigation.ui)
+    implementation(libs.androidx.core.navigation.compose)
+    implementation(libs.androidx.core.navigation.fragment)
+    implementation(libs.androidx.core.navigation.hilt)
+
+
+    debugImplementation(platform(libs.compose.bom))
+    debugImplementation(libs.compose.ui.test.manifest)
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.square.leakcanary)
+
+    annotationProcessor(libs.androidx.room.compiler)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.compose.ui.test.junit)
+    androidTestImplementation(libs.hilt.android.testing)
+
+
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,48 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+
+    dependencies {
+        classpath(libs.detekt.gradle.plugin)
+        classpath(libs.gradle)
+        classpath(libs.gradle.versions.plugin)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+
+
+
+    }
+
+    configurations.classpath {
+        resolutionStrategy {
+            force(
+                "com.pinterest.ktlint:ktlint-rule-engine:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-rule-engine-core:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-core:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-checkstyle:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-json:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-html:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-plain:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-sarif:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-ruleset-standard:${libs.versions.ktlint.get()}",
+            )
+        }
+    }
 }
+
+plugins {
+    id("com.google.dagger.hilt.android").version(libs.versions.hilt).apply(false)
+    id("io.gitlab.arturbosch.detekt").version(libs.versions.detektGradlePlugin)
+    id("org.jmailen.kotlinter").version(libs.versions.kotlinter).apply(false)
+    id("com.google.devtools.ksp").version(libs.versions.ksp).apply(false)
+    id("com.squareup.sort-dependencies").version(libs.versions.sortDependencies).apply(false)
+}
+
+
+
+
+
