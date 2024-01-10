978724e4c70c09d1ed1cc517c5ee39aa1ed9bb69
Sliweczkowa <sliweczkowasliweczka@outlook.com>
2024-01-10 10:30:53 +0100

    feat: hilt and viewModel

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.helloworld.ui.theme.HelloWorldTheme
 import com.example.helloworld.visuals.Edit.ModifyScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/helloworld/visuals/Edit/ModifyViewModel.kt
+++ app/src/main/java/com/example/helloworld/visuals/Edit/ModifyViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.helloworld.visuals.Edit
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.helloworld.data.PlaceRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,5 +13,31 @@ import javax.inject.Inject
 class ModifyViewModel @Inject constructor(
     private val placeRepo: PlaceRepo) : ViewModel() {
 
+        var name by mutableStateOf("")
+        private set
+        var date by mutableStateOf("")
+        private set
+        var rating by mutableStateOf("")
+        private set
+        var description by mutableStateOf("")
+        private set
+        var photoPath by mutableStateOf("")
+        private set
 
-    }
+        fun onNameChange(name: String) {
+            this.name = name
+        }
+        fun onDateChange(date: String) {
+            this.date = date
+        }
+        fun onratingChange(rating: String) {
+            this.rating = rating
+        }
+        fun ondDscriptionChange(description: String) {
+            this.description = description
+        }
+        fun onPhotoPathChange(photoPath: String) {
+            this.photoPath = photoPath
+        }
+
+}
