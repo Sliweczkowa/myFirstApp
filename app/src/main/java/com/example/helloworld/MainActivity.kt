a0f12d06f8710c42e8671d9fada54f334af2b662
Wiktoria Śliwińska <242231@edu.p.lodz.pl>
2024-01-31 03:51:56 +0100

    feat: navigation

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -6,8 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.helloworld.domain.util.Screen
 import com.example.helloworld.ui.theme.HelloWorldTheme
 import com.example.helloworld.visuals.Edit.ModifyScreen
+import com.example.helloworld.visuals.Home.HomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -18,8 +25,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloWorldTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-//                    HomeScreen()
-                    ModifyScreen(this)
+                    val con = this
+
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.HomeScreen.route,
+                    ) {
+                        composable(route = Screen.HomeScreen.route) {
+                            HomeScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.ModifyScreen.route +
+                                    "?placeId={placeId}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "placeId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                            )
+                        ) {
+                            ModifyScreen(
+                                context = con,
+                                navController = navController
+                            )
+                        }
+                    }
                 }
             }
         }
