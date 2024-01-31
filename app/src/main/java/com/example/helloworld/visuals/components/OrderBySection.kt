a6ea0e5dace220286961503067e3252848bd709a
Wiktoria Śliwińska <242231@edu.p.lodz.pl>
2024-01-31 04:37:27 +0100

    chore: deleted unused file, imports, restyled

--- app/src/main/java/com/example/helloworld/visuals/components/OrderBySection.kt
+++ app/src/main/java/com/example/helloworld/visuals/components/OrderBySection.kt
@@ -3,7 +3,6 @@ package com.example.helloworld.visuals.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
