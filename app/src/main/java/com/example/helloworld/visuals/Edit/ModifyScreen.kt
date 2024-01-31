afdf18c5fe5ccae536f504d2a683ebf2f3ab2ffc
Wiktoria Śliwińska <242231@edu.p.lodz.pl>
2024-01-31 03:18:01 +0100

    fix: added keyboard imports

--- app/src/main/java/com/example/helloworld/visuals/Edit/ModifyScreen.kt
+++ app/src/main/java/com/example/helloworld/visuals/Edit/ModifyScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.DateRange
@@ -32,6 +33,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
